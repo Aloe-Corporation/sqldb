d35558e679ba889ea76668a97380fa3d983e44c9
Florian Richard <florian.richard@aloe-corp.com>
2023-12-04 14:50:45 +0100

    Remove docker manager related files, test refactoring

--- test/test.go
+++
@@ -1,9 +0,0 @@
-package test
-
-import "os"
-
-var (
-	WorkDir            = os.Getenv("GOPATH") + "/src/github.com/Aloe-Corporation/sqldb"
-	PathScriptPostgres = WorkDir + "/test/schema_postgres.sql"
-	PathScriptMySQL    = WorkDir + "/test/schema_mysql.sql"
-)
